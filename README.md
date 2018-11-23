2686cbdbbefed13c74ef8e75f92db6f841ac649e
Dan Simmons <danieljsimmons@btinternet.com>
2018-11-23 09:45:49 +0000

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # MapAdmin
-version: 0.9 - not yet ready - not releaased
+version: 0.9.01 - not yet ready - not released
 ---
 
 ## Description and overview
@@ -9,21 +9,21 @@ Provides a Browser based dashboard with MapView, for managing ORCL mapdata for L
  - Import a zipped set of shapefiles as a map
  - filterable and sortable tabular view of features with their RelatedData
  - highlight filtered set of features on a map
-  -  Inspect explor Map data in a tree view of nodes
+  -  Inspect explore Map data in a tree view of nodes
   -  Export mapdata CSV or as ShapeFile set
-  -  Upload Dataset to the cloud database, as a new map 
+  -  Upload Dataset to the cloud database, as a new map
   -  User Not logged in can access one set of webpages (Read only), user logged in can access additionally MapAdmin page (read and write to database)
+   - set map bounds (FlyTo) to a selected feature on the table
 
 ## Bugs / issues
  - large dataset can freeze UI while busy - how to interrupt
- - how to report 'busy' status to user while canvas is busy when redrawing large datasets on Leaflet map 
+ - how to report 'busy' status to user while canvas is busy when redrawing large datasets on Leaflet map
  - Visual style and layout  - replace MapAdmin tabs with menu
- - Add company branding /logo /visual style 
+ - Add company branding /logo /visual style
  - Addess 'bundle much too large' message
 
 ## features todo / wishlist
-  - Set map bounds to extent of selectedRows (with Minimum bounds)
-  - Ability to view/inspect feature properties upon selecting a map feature (ie feature_on_click detailed infobox)
+
   - Create webpage content for public static Webpages
 
 ## Notes
@@ -44,7 +44,7 @@ Provides a Browser based dashboard with MapView, for managing ORCL mapdata for L
 
 #### onSelectAll_clicked
     for EachRow in FilteredSetOfRows
-      // Row Index corresponds directly to index of feature in Map GeoJson Data 
+      // Row Index corresponds directly to index of feature in Map GeoJson Data
       set highlightOnMap property of MapDataFeature element RowIndex to TRUE
 
 #### onSelectNone_clicked
@@ -54,7 +54,7 @@ Provides a Browser based dashboard with MapView, for managing ORCL mapdata for L
 
 #### onTableRowClick:
     toggle the value of mapAdmin.state.geojson.features(tableRow.index).properties.highlight_on_map
-    // then the change of state causes react to refresh style prop callback forEachFeature 
+    // then the change of state causes react to refresh style prop callback forEachFeature
 
 
 ## How it it built - technology stack
@@ -66,7 +66,7 @@ Provides a Browser based dashboard with MapView, for managing ORCL mapdata for L
 ---
 
 # Proposed Client WebMap View
-## User requirement 
+## User requirement
 - Ability to easily search for feature (or set of features), and  highlight on map,  by Condition, featureName, or Memorial Plaque details / memorial name
  - Apply bookmarks (extents or map bounds), if map based on ward and/or park name(s)
  - Ability to see who (person or organisation) owns/manages an asset
@@ -76,19 +76,18 @@ Provides a Browser based dashboard with MapView, for managing ORCL mapdata for L
 
 
 ### User Stories
- - As a GIS user / client end-user I want to be able to respond to a customer enquiry, (from my office computer), asking me where abouts in a park they can find their memorial bench (or memorial tree), by intuatively searching/inspecting an interactive WebMap
+ - As a GIS user / client end-user I want to be able to respond to a customer enquiry, (from my office computer), asking me where abouts in a park they can find their memorial bench (or memorial tree), by intuitively searching/inspecting an interactive WebMap
 
  - As a GIS user / client end-user I want to easily find the information on who owns (or manages) a specific feature that I have identified on the WebMap (eg a specific boundary wall.
 
  - (As a 'friend of park' (or as member of the public?) I want to identify the location of my memorial bench (or memorial tree), from an easily-usable WebMap, from my phone, tablet or computer
- 
-### Proposed map design behavour for 'who owns/manages what' map 
+
+### Proposed map design behavour for 'who owns/manages what' map
   Start with map containing all features of the park Iie basic Leaflet -  Click feature on map showing all features over OpenMap baselayer to view popup infoBox containing details inc who Owns/manages, (partly solved in WebApp, however would need to search table)
 
 
 ### For map for searching for a specific memorial etc
 
-    Start with empty map (with minimal detail eg basebap with park bounds?) with search bar - generate a dropdownlist of fuzzy results from search, and display (and zoom to), selected (clicked) result, on map.  - maybe use google mymap?
+    Start with empty map (with minimal detail eg basemap with park bounds?) with search bar - generate a dropdownlist of fuzzy results from search, and display (and zoom to), selected (clicked) result, on map.  - maybe use google mymap?
 
 ### Combination of above two maps
-  tyf
--- src/components/TableView.js
+++ src/components/TableView.js
@@ -9,79 +9,72 @@ import "./table-view.css";
 import treeTableHOC from "react-table/lib/hoc/treeTable";
 import { getRelatedData } from "../firebase/firebase";
 import ImageLoader from "react-load-image";
-import {
-    Button,
-    Row
-} from "reactstrap";
+import { Button, Row } from "reactstrap";
 
 const TreeTable = treeTableHOC(ReactTable);
 
-
-
 const Photo = props => {
-	return (
-		<div>
-			<ImageLoader src={props.url}>
-				<img style={{width: '100%'}} />
-			</ImageLoader>
-			The logged in user is {props.url}
-		</div>
-	);
+  return (
+    <div>
+      <ImageLoader src={props.url}>
+        <img style={{ width: "100%" }} />
+      </ImageLoader>
+      The logged in user is {props.url}
+    </div>
+  );
 };
 
 // NOTES - geoJson set in constructor only - so won't add new related data in realtime
 
 class TableView extends React.Component {
-	constructor(props) {
-		super(props);
-		//this.handleRowClick = this.handleRowClick.bind(this);
-		this.handleRowClick2 = this.handleRowClick2.bind(this);
-		this.selectAllRows = this.selectAllRows.bind(this)
-		this.selectNoRows = this.selectNoRows.bind(this)
-		this.locateFeatureOnMap = this.locateFeatureOnMap.bind(this)
-		//this.cancelled = false		
-		//this.handleFilterUpdate = this.handleFilterUpdate.bind(this)
-		//this.reactFilteredTable = this.reactFilteredTable.bind(this);
-		//const p = this.props.data[0].properties;
+  constructor(props) {
+    super(props);
+    //this.handleRowClick = this.handleRowClick.bind(this);
+    this.handleRowClick2 = this.handleRowClick2.bind(this);
+    this.selectAllRows = this.selectAllRows.bind(this);
+    this.selectNoRows = this.selectNoRows.bind(this);
+    this.locateFeatureOnMap = this.locateFeatureOnMap.bind(this);
+    //this.cancelled = false
+    //this.handleFilterUpdate = this.handleFilterUpdate.bind(this)
+    //this.reactFilteredTable = this.reactFilteredTable.bind(this);
+    //const p = this.props.data[0].properties;
 
-		this.state = {
-			col1: "default",
-			col2: "default",
-			col3: "default",
-			activeRowCoords: [],
-			expanded: {},
-			activeRow: null,
-			selectedStyle: { backgroundColor: "khaki" },
-			unSelectedStyle: { backgroundColor: "gainsboro" },
-			tableData: this.props.data,
-			selectedRows : []
-		};
-	}
+    this.state = {
+      col1: "default",
+      col2: "default",
+      col3: "default",
+      activeRowCoords: [],
+      expanded: {},
+      activeRow: null,
+      selectedStyle: { backgroundColor: "khaki" },
+      unSelectedStyle: { backgroundColor: "gainsboro" },
+      tableData: this.props.data,
+      selectedRows: []
+    };
+  }
 
-	//var keys = {};
-     //window.onkeyup = function(e) { keys[e.keyCode] = false; }
-   
+  //var keys = {};
+  //window.onkeyup = function(e) { keys[e.keyCode] = false; }
 
-	componentWillMount() {
-		//console.log("hello from cwm")
-		//document.addEventListener("keydown", this._handleKeyDown.bind(this));
-		//this.assignRelatedDataToTableData();
+  componentWillMount() {
+    //console.log("hello from cwm")
+    //document.addEventListener("keydown", this._handleKeyDown.bind(this));
+    //this.assignRelatedDataToTableData();
 
-		this.setState({
-			//newpropertiesList
-			//myProps
-		});
-	}
- 
-    /*
+    this.setState({
+      //newpropertiesList
+      //myProps
+    });
+  }
+
+  /*
     _handleKeyDown = (event) => {
        this.cancelled=true
        console.log("keyDown!")
     }
     */
 
-
-	/*
+  /*
 	assignRelatedDataToTableData() {
 		// fetch latest relatedDataSet for map
 		getRelatedData("myKey");
@@ -91,50 +84,50 @@ class TableView extends React.Component {
 	}
 	*/
 
-	/*
+  /*
 	componentDidUpdate(){
 		console.log("tableInstance (from cdUpdate):", this.tableInstance.getWrappedInstance().getResolvedState().sortedData);
 	}
 	*/
 
-	componentWillReceiveProps() {
-		console.log("receive Props!", Date());
-		let p = this.props.data[0].properties; // base the headings on fields in the  1st record
+  componentWillReceiveProps() {
+    console.log("receive Props!", Date());
+    let p = this.props.data[0].properties; // base the headings on fields in the  1st record
 
-		const col1 = Object.keys(p).find(
-			e =>
-				e === "name" ||
-				e === "Name" ||
-				e === "NAME" ||
-				e === "Asset" ||
-				e === "ASSET" ||
-				e === "OBJECTID"
-		);
-		const col2 = Object.keys(p).find(
-			e => e === "description" || e === "DESCRIPTIO"
-		);
+    const col1 = Object.keys(p).find(
+      e =>
+        e === "name" ||
+        e === "Name" ||
+        e === "NAME" ||
+        e === "Asset" ||
+        e === "ASSET" ||
+        e === "OBJECTID"
+    );
+    const col2 = Object.keys(p).find(
+      e => e === "description" || e === "DESCRIPTIO"
+    );
 
-		const newPropKey = this.state.tableData[0].myNewProp;
-		this.setState({
-			col1: col1,
-			col2: col2,
-			tableData: this.props.data
-		});
-	}
+    const newPropKey = this.state.tableData[0].myNewProp;
+    this.setState({
+      col1: col1,
+      col2: col2,
+      tableData: this.props.data
+    });
+  }
 
-	/*
+  /*
 	handleRowClick(e, handleOriginal, rowInfo, state, instance) {
 		//console.log("instance (RowClick):", instance.getResolvedState().sortedData);
 		this.props.rowCallback(rowInfo, handleOriginal);
 	}
 	*/
 
-	handleRowClick2(row) {
-		//console.log("instance (RowClick):", instance.getResolvedState().sortedData);
-		this.props.rowCallback2(row);
-	}
+  handleRowClick2(row) {
+    //console.log("instance (RowClick):", instance.getResolvedState().sortedData);
+    this.props.rowCallback2(row);
+  }
 
-	/*
+  /*
 	myCallback (){
 		console.log("callback called!")
 		this.cancelled = true
@@ -143,29 +136,34 @@ class TableView extends React.Component {
 	}
 	*/
 
-	handleFilterUpdate(){
-		console.log("tableInstance:", this.tableInstance.getWrappedInstance().getResolvedState().sortedData);
-		this.setState({selectedRows :  this.tableInstance.getWrappedInstance().getResolvedState().sortedData})
-	
-	//console.log("start")
-	//this.cancelled= false
-	//console.log("cancelled:" , this.cancelled)
-	/*
+  handleFilterUpdate() {
+    console.log(
+      "tableInstance:",
+      this.tableInstance.getWrappedInstance().getResolvedState().sortedData
+    );
+    this.setState({
+      selectedRows: this.tableInstance.getWrappedInstance().getResolvedState()
+        .sortedData
+    });
+
+    //console.log("start")
+    //this.cancelled= false
+    //console.log("cancelled:" , this.cancelled)
+    /*
 	for (let i = 0; i<100000000; i++){
 			//console.log(this.cancelled)
 			if (this.cancelled == true) {
 				console.log("interupted and break!")
-				break} 
+				break}
 			}
 	*/
-	//this.cancelled = false
-	//console.log("finish", "cancelled:", this.cancelled)		
-	}
+    //this.cancelled = false
+    //console.log("finish", "cancelled:", this.cancelled)
+  }
 
-
-	/*
+  /*
 	handleRowExpanded(newExpanded, index, event) {
-	
+
 		this.setState({
 			// we override newExpanded, keeping only current selected row expanded
 			expanded: { [index]: !this.state.expanded[index] }
@@ -177,13 +175,13 @@ class TableView extends React.Component {
 	}
 	*/
 
+  showRelated(value) {
+    console.log("showRelated clicked!", value);
+    
+  }
 
-	showRelated(value) {
-		console.log("showRelated clicked!", value);
-	}
-
-	selectAllRows (){
-		/*
+  selectAllRows() {
+    /*
 		const selectedRows = this.state.selectedRows
 		selectedRows.map(row => {
 			//this.state.tableData[row._index].properties.highlightOnMap = true
@@ -196,42 +194,40 @@ class TableView extends React.Component {
 
 		  //this.setState({ state: this.state });
 		*/
-	//console.log("selectAllRows Button!")
-	
+    //console.log("selectAllRows Button!")
 
-	this.props.selectAllRowsCallback (this.state.selectedRows)
-	}
+    this.props.selectAllRowsCallback(this.state.selectedRows);
+  }
 
-	selectNoRows (){
-		//const selectedRows = this.state.selectedRows
-		/*
+  selectNoRows() {
+    //const selectedRows = this.state.selectedRows
+    /*
 		this.state.tableData.map(row => {
 			row.properties.highlightOnMap = false
 			//row.properties.highlightOnMap = true
 		})
 		*/
-		this.props.selectNoRowsCallback ()
-		//console.log("select AllRows!",)
-	}
+    this.props.selectNoRowsCallback();
+    //console.log("select AllRows!",)
+  }
 
-	locateFeatureOnMap (featureIndex){
-		//console.log("locateONMap!",e)
-		this.props.zoomToFeatureCallback (featureIndex)
-	}
+  locateFeatureOnMap(featureIndex) {
+    //console.log("locateONMap!",e)
+    this.props.zoomToFeatureCallback(featureIndex);
+  }
 
-
-	render() {
-		return (
-			<div>
-			<Button onClick={this.selectAllRows}>Select All</Button>
-			<Button onClick={this.selectNoRows}>Clear all</Button>
-			<TreeTable
-				data={this.state.tableData}
-				minRows={1}
-				showPaginationTop = {true}
-				showPaginationBottom = {false}
-				ref={(instance)=>this.tableInstance=instance}
-				/*
+  render() {
+    return (
+      <div>
+        <Button onClick={this.selectAllRows}>Select All</Button>
+        <Button onClick={this.selectNoRows}>Clear all</Button>
+        <TreeTable
+          data={this.state.tableData}
+          minRows={1}
+          showPaginationTop={true}
+          showPaginationBottom={false}
+          ref={instance => (this.tableInstance = instance)}
+          /*
 				onExpandedChange={
 					(newExpanded, index, event) =>{
 						console.log("expandedChanged!")
@@ -239,110 +235,120 @@ class TableView extends React.Component {
 					}
 				} */
 
-				onFilteredChange={() => {
-    				//const { page } = this.tableInstance.getResolvedState()
-    				// page is the current pageIndex
-    				//console.log("filteredChanged!", this.tableInstance.getWrappedInstance().getResolvedState().sortedData)
-  				    this.handleFilterUpdate()
-  					//this.myCallback()
-  				}}
-				getTdProps={(state, rowInfo, column, instance) => {
-					return {
-						style:
-							rowInfo && this.state.tableData[rowInfo.index]
-								? this.state.tableData[rowInfo.index].properties
-										.highlightOnMap
-									? this.state.selectedStyle
-									: this.state.unSelectedStyle
-								: this.state.unSelectedStyle				
-					};
-				}}
-				SubComponent={row => {
-					// a SubComponent just for the final detail
+          onFilteredChange={() => {
+            //const { page } = this.tableInstance.getResolvedState()
+            // page is the current pageIndex
+            //console.log("filteredChanged!", this.tableInstance.getWrappedInstance().getResolvedState().sortedData)
+            this.handleFilterUpdate();
+            //this.myCallback()
+          }}
+          getTdProps={(state, rowInfo, column, instance) => {
+            return {
+              style:
+                rowInfo && this.state.tableData[rowInfo.index]
+                  ? this.state.tableData[rowInfo.index].properties
+                      .highlightOnMap
+                    ? this.state.selectedStyle
+                    : this.state.unSelectedStyle
+                  : this.state.unSelectedStyle
+            };
+          }}
+          SubComponent={row => {
+            // a SubComponent just for the final detail
 
-					const columns = [
-						{
-							Header: "Property",
-							accessor: "property",
-							style: {
-								backgroundColor: "#DDD",
-								textAlign: "right"
-							}
-						},
-						{
-							Header: "Value",
-							accessor: "value"
-						}
-					];
+            const columns = [
+              {
+                Header: "Property",
+                accessor: "property",
+                style: {
+                  backgroundColor: "#DDD",
+                  textAlign: "right"
+                }
+              },
+              {
+                Header: "Value",
+                accessor: "value"
+              }
+            ];
 
-					const rowData = Object.keys(row.original.properties).map(
-						key => {
-							return {
-								property: key,
-								value: row.original.properties[key].toString()
-							};
-						}
-					);
+            const rowData = Object.keys(row.original.properties).map(key => {
+              return {
+                property: key,
+                value: row.original.properties[key].toString()
+              };
+            });
 
-					let photoUrl = row.original.properties['photo'] || "http://placekitten.com/300/150" ;
- 
-					return (
-						<div>
-						<Row>
-						<Button color="primary" size="sm" className="col-sm-3"  onClick={(e)=>{
-							this.locateFeatureOnMap(row.index)
-							}}>Fly to
-						</Button>
-						<Button color="info" size="sm" className="col-sm-3 offset-sm-1" onClick={(e)=>{
-							  this.handleRowClick2( row	);
-						   }}>Show / Hide
-						</Button>
-						</Row>
-							<Photo url={photoUrl} />
-							<ReactTable
-								data={rowData}
-								columns={columns}
-								minRows={1}				
-							/>
-						</div>
-					);
-				}}
-				columns={[
-					{
-						Header: this.state.col1,
-						accessor: "properties." + this.state.col1
-					},
-					{
-						Header: this.state.col2,
-						accessor: "properties." + this.state.col2,
-						maxWidth: 100
-					},
-					{
-						Header: "Condition",
-						id: "condition",
-						accessor: "properties.condition",
-						maxWidth: 100
-					},
-					{
-						Header: "",
-						id: "highlightOnMap",
-						accessor: "properties.highlightOnMap",
-						show: false
-					}
-				]}
-				defaultPageSize="5"
-				className="-striped -highlight"
-				collapseOnDataChange =  {false}
-				filterable
-				showPageJump = {false}
-				defaultFilterMethod={(filter, row, column) => {
-					const id = filter.pivotId || filter.id;
-					return  String(row[id]).toLowerCase().includes(filter.value.toLowerCase())
-				}}
-			/>
-			</div>
-		);
-	}
+            let photoUrl =
+              row.original.properties["photo"] ||
+              "http://placekitten.com/300/150";
+
+            return (
+              <div>
+                <Row>
+                  <Button
+                    color="primary"
+                    size="sm"
+                    className="col-sm-3"
+                    onClick={e => {
+                      this.locateFeatureOnMap(row.index);
+                    }}
+                  >
+                    Fly to
+                  </Button>
+                  <Button
+                    color="info"
+                    size="sm"
+                    className="col-sm-3 offset-sm-1"
+                    onClick={e => {
+                      this.handleRowClick2(row);
+                    }}
+                  >
+                    Show / Hide
+                  </Button>
+                </Row>
+                <Photo url={photoUrl} />
+                <ReactTable data={rowData} columns={columns} minRows={1} />
+              </div>
+            );
+          }}
+          columns={[
+            {
+              Header: this.state.col1,
+              accessor: "properties." + this.state.col1
+            },
+            {
+              Header: this.state.col2,
+              accessor: "properties." + this.state.col2,
+              maxWidth: 100
+            },
+            {
+              Header: "Condition",
+              id: "condition",
+              accessor: "properties.condition",
+              maxWidth: 100
+            },
+            {
+              Header: "",
+              id: "highlightOnMap",
+              accessor: "properties.highlightOnMap",
+              show: false
+            }
+          ]}
+          defaultPageSize="5"
+          className="-striped -highlight"
+          collapseOnDataChange={false}
+          filterable
+          showPageJump={false}
+          defaultFilterMethod={(filter, row, column) => {
+            const id = filter.pivotId || filter.id;
+            return String(row[id])
+              .toLowerCase()
+              .includes(filter.value.toLowerCase());
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default TableView;
