f3a2a360df6921a36800c46c282c51e2aeacd1cb
Dan Simmons <danieljsimmons@btinternet.com>
2019-07-02 12:06:08 +0100

    added new proj code from asus etc

--- src/components/UploadNewMap.js
+++ src/components/UploadNewMap.js
@@ -31,12 +31,20 @@ class UploadNewMap extends React.Component {
     super(props);
     //this.handleSubmit = this.handleSubmit.bind(this);
     this.uploadNewMap = this.uploadNewMap.bind(this);
+    this.createNewProject = this.createNewProject.bind(this);
+    this.uploadNewBlankMapForRelData = this.uploadNewBlankMapForRelData.bind(
+      this
+    );
     this.state = {
       mapIndeces: [{ name: "Freddy", age: 27 }, { name: "Jimmy", age: 25 }],
       mapName: "",
       mapDescription: "",
       relDataMapHash: "",
-      projectHash: ""
+      projectHash: "",
+      projectName: "",
+      projectCompletedResetDate: "",
+      projectDescription: "",
+      projectMapSet: []
     };
   }
 
@@ -91,10 +99,56 @@ class UploadNewMap extends React.Component {
       });
   }
 
+  createNewProject() {
+    console.log("createNewProject called!");
+    // Project object factory
+    const projectOb = {
+      name: this.state.projectName,
+      description: this.state.projectDescription,
+      completedResetDate: this.state.projectCompletedResetDate // todo format ??!!
+      // dont set mapSet here - instead create node onUploadNewMap if none exists
+    };
+    // push new project and retrive key
+    /*
+    const myVar1 = db
+      .ref("App/Projects/")
+      .push({ prop2: "propval2" })
+      .then(snap => {
+        console.log("key:", snap.key);
+        return "Hello!";
+      });
+    console.log("myVar1:", myVar1.PromiseValue);
+  */
+
+    const newRef = db.ref("App/Projects/").push(projectOb);
+    console.log("newRefKey:", newRef.key);
+    this.setState({ projectHash: newRef.key });
+    this.uploadNewBlankMapForRelData(newRef.key);
+  }
+
+  uploadNewBlankMapForRelData(relDataMapHash) {
+    const myOb = {
+      config: {
+        relDataMapHash: ""
+      }
+    };
+    const newRef = db.ref("App/Maps/").push(myOb);
+    console.log("newRefKey:", newRef.key);
+
+    console.log("rel setting:", `App/Maps/${newRef.key}/config/relDataMapHash`);
+    db.ref(`App/Maps/${newRef.key}/config/relDataMapHash`).set(newRef.key);
+    this.setState ({relDataMapHash:newRef.key})
+  }
+
   render() {
     return (
       <div>
         <h4>Upload the current data as new map on database </h4>
+        <p>
+        Upload the map to an existing project (copy and paste map hash and relDataMap hash).</p>
+        <p>Or add to a new map - by creating a new project (below), which populates mapHash and relDataMap hass with
+        newly created entries.
+        </p>
         <Form>
           <FormGroup>
             <Label for="mapName">Map name</Label>
@@ -153,6 +207,58 @@ class UploadNewMap extends React.Component {
           >
             Upload
           </Button>
+        </Form>
+        <hr />
+        <h3>Create new project</h3>
+        <p>
+        Create a project name, description and set a reset Date (for any related data)
+        then upload maps against that project
+        </p>
+        <Form>
+          <FormGroup>
+            <Label>Project name</Label>
+            <Input
+              type="text"
+              name="projectName"
+              id="projectNameEl"
+              placeholder="Project name"
+              value={this.state.projectName}
+              onChange={e => {
+                this.setState({ projectName: e.target.value });
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Project description</Label>
+            <Input
+              type="text"
+              name="projectdescription"
+              id="projectDescriptionEl"
+              placeholder="Project desciption"
+              value={this.state.projectDescription}
+              onChange={e => {
+                this.setState({ projectDescription: e.target.value });
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Project completion reset date</Label>
+            <Input
+              type="text"
+              name="completedResetDate"
+              id="completedResetDateEl"
+              placeholder="10 July 2018"
+              value={this.state.projectCompletedResetDate}
+              onChange={e => {
+                this.setState({ projectCompletedResetDate: e.target.value });
+              }}
+            />
+          </FormGroup>
+          <p>
+            <Button color="success" onClick={this.createNewProject}>
+              Create new project
+            </Button>
+          </p>
         </Form>
       </div>
     );
