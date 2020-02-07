29d254d1dce8314a32597f7a79d12b120d1af04e
Dan Simmons <danieljsimmons@btinternet.com>
2020-02-07 15:55:39 +0000

    export markers to GeoJson feature collection

--- src/components/ExportMarkers.js
+++ src/components/ExportMarkers.js
@@ -13,7 +13,29 @@ class ExportMarkers extends React.Component {
   }
 
   handleSaveButton() {
-    console.log("save button called with geoJson:", this.props.geoJson);
+    //console.log("save button called with geoJson:", this.props.geoJson);
+    //const markerObset = this.props.geoJson;
+    //const featureArray = markerObset.map(markerOb => {
+    //  return Object.values(markerOb)[0];
+    //});
+
+    const download = (content, fileName, contentType) => {
+      console.log("download!");
+      const a = document.createElement("a");
+      const contentText = JSON.stringify(content);
+      const file = new Blob([contentText], { type: contentType });
+      a.href = URL.createObjectURL(file);
+      a.download = fileName;
+      a.click();
+    };
+
+    const featureArray = Object.values(this.props.geoJson);
+    console.log("Marker Array:", featureArray);
+    const featureCollection = {};
+    featureCollection.type = "FeatureCollection";
+    featureCollection.features = featureArray;
+    download(featureCollection, "myMarkers.json", "text/plain");
+
     //this.saveAsShape(this.props.geoJson, "download_shp");
   }
 
